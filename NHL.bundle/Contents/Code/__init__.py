3a442dacfdad82e8f732f2cf474c5cc257178888
pudds55@gmail.com <pudds55@gmail.com@b505dfd5-760f-10fe-571a-1d0aa5fc3d89>
2013-10-03 19:22:49 +0000

    move team constants so we can reuse in service

--- NHL.bundle/Contents/Code/__init__.py
+++ NHL.bundle/Contents/Code/__init__.py
@@ -1,4 +1,5 @@
 import sports_streams_core as core
+import teams
 import datetime
 from dateutil import tz
 
@@ -14,38 +15,6 @@ ICON = 'icon-default.png'
 SPORT_KEYWORD = "hockey"
 STREAM_FORMAT = "http://nlds{server}.cdnak.neulion.com/nlds/nhl/{streamName}/as/live/{streamName}_hd_{q}.m3u8"
 
-TEAMS = {
-	"ANA": { "City": "Anaheim", "Name": "Ducks", "Logo": R("Team_ANA.jpg") },
-	"BOS": { "City": "Boston", "Name": " Bruins", "Logo": R("Team_BOS.jpg") },
-	"BUF": { "City": "Buffalo", "Name": " Sabres", "Logo": R("Team_BUF.jpg") },
-	"CAR": { "City": "Carolina", "Name": " Hurricanes", "Logo": R("Team_CAR.jpg") },
-	"CBS": { "City": "Columbus", "Name": " Blue Jackets", "Logo": R("Team_CBS.jpg") },
-	"CGY": { "City": "Calgary", "Name": " Flames", "Logo": R("Team_CGY.jpg") },
-	"CHI": { "City": "Chicago", "Name": " Blackhawks", "Logo": R("Team_CHI.jpg") },
-	"COL": { "City": "Colorado", "Name": " Avalanche", "Logo": R("Team_COL.jpg") },
-	"DAL": { "City": "Dallas", "Name": " Stars", "Logo": R("Team_DAL.jpg") },
-	"DET": { "City": "Detroit", "Name": " Red Wings", "Logo": R("Team_DET.jpg") },
-	"EDM": { "City": "Edmonton", "Name": " Oilers", "Logo": R("Team_EDM.jpg") },
-	"FLA": { "City": "Florida", "Name": " Panthers", "Logo": R("Team_FLA.jpg") },
-	"LOS": { "City": "Los Angeles", "Name": " Kings", "Logo": R("Team_LOS.jpg") },
-	"MIN": { "City": "Minnesota", "Name": " Wild", "Logo": R("Team_MIN.jpg") },
-	"MON": { "City": "Montreal", "Name": " Canadiens", "Logo": R("Team_MON.jpg") },
-	"NJD": { "City": "New Jersey", "Name": " Devils", "Logo": R("Team_NJD.jpg") },
-	"NSH": { "City": "Nashville", "Name": " Predators", "Logo": R("Team_NSH.jpg") },
-	"NYI": { "City": "NY", "Name": " Islanders", "Logo": R("Team_NYI.jpg") },
-	"NYR": { "City": "NY", "Name": " Rangers", "Logo": R("Team_NYR.jpg") },
-	"OTT": { "City": "Ottawa", "Name": " Senators", "Logo": R("Team_OTT.jpg") },
-	"PHI": { "City": "Philadelphia", "Name": " Flyers", "Logo": R("Team_PHI.jpg") },
-	"PHX": { "City": "Phoenix", "Name": " Coyotes", "Logo": R("Team_PHX.jpg") },
-	"PIT": { "City": "Pittsburgh", "Name": " Penguins", "Logo": R("Team_PIT.jpg") },
-	"SAN": { "City": "San Jose", "Name": " Sharks", "Logo": R("Team_SAN.jpg") },
-	"STL": { "City": "St. Louis", "Name": " Blues", "Logo": R("Team_STL.jpg") },
-	"TAM": { "City": "Tampa Bay", "Name": " Lightning", "Logo": R("Team_TAM.jpg") },
-	"TOR": { "City": "Toronto", "Name": " Maple Leafs", "Logo": R("Team_TOR.jpg") },
-	"VAN": { "City": "Vancouver", "Name": " Canucks", "Logo": R("Team_VAN.jpg") },
-	"WPG": { "City": "Winnipeg", "Name": " Jets", "Logo": R("Team_WPG.jpg") },
-	"WSH": { "City": "Washington", "Name": " Capitals", "Logo": R("Team_WSH.jpg") }
-}
 
 ###############################################
 
@@ -59,7 +28,7 @@ def Start():
 	
 	ObjectContainer.title1 = NAME
 	
-	core.Init(NAME, SPORT_KEYWORD, STREAM_FORMAT, TEAMS)
+	core.Init(NAME, SPORT_KEYWORD, STREAM_FORMAT, teams.TEAMS)
 	
 	Log.Debug("Plugin Start")
 
